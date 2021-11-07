ba386076f0d685cefade1c874462b6064f94e976
hiro881213 <nikep.godbard@gmail.com>
2021-11-07 22:05:01 +0900

    PaperJS読み込み

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 $(document).ready(() => {
 
     'use strict';
-    
+
+    // ペーパーを読み込む
     paper.install(window);
+
+    // 対象タグを読み込む
     paper.setup(document.getElementById("paperCanvas"));
 
-    // 形状を生成する
+    // 丸を生成する
     let c = Shape.Circle(200, 200, 50);
+
+    // 色を塗る
     c.fillColor = 'green';
 
+    // 図形を描画する
     paper.view.draw();
 
 });
