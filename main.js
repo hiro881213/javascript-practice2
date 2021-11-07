0c5e1abf32ac71a4e8c74ace530cabec7a905a0d
hiro881213 <nikep.godbard@gmail.com>
2021-11-07 22:12:35 +0900

    繰り返し

--- main.js
+++ main.js
@@ -14,7 +14,26 @@ $(document).ready(() => {
     // 色を塗る
     c.fillColor = 'green';
 
+    // 対象タグを読み込む
+    paper.setup(document.getElementById("repeatCanvas"));
+
+    // 繰り返し処理を実行する
+    for (let x = 25; x < 400; x+=50) {
+
+        for (let y = 25; y < 400; y+=50) {
+
+            // 円を生成する
+            c = Shape.Circle(x, y, 20);
+
+            // 色を塗る
+            c.fillColor = 'green';
+
+        }
+
+    }
+
     // 図形を描画する
     paper.view.draw();
+
 
 });
