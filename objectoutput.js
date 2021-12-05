3fc35694c8167da914bfbce15c71a9eee4035b8e
hiro881213 <nikep.godbard@gmail.com>
2021-12-05 22:09:41 +0900

    配列

--- objectoutput.js
+++ objectoutput.js
@@ -55,4 +55,30 @@ function outputLogData() {
 
     console.log(s2);
 
+    const a1 = [1, 2, 3, 4];
+
+    const a2 = [1, 'two', 3, null];
+
+    const a3 = [
+        "What the hammer? What the chain?",
+        "In what furnace was thy brain?",
+        "What the anvil? What dread grasp",
+        "dare its deadly terrors clasp?",
+    ];
+
+    const a4 = [
+        {name: "Ruby", hardness: 9},
+        {name: "Diamond", hardness: 10},
+        {name: "Topaz", hardness: 8}
+    ];
+
+    const a5 = [
+        [1, 3, 5],
+        [2, 4, 6]
+    ];
+
+    console.log(a1.length);
+    console.log(a1[0]);
+    console.log(a3[a3.length - 2]);
+
 }
