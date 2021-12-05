96b53efa60f45ac9c917cd81d2aadb5037510a31
hiro881213 <nikep.godbard@gmail.com>
2021-12-05 21:43:42 +0900

    オブジェクト学習

--- objectoutput.js
+++ objectoutput.js
@@ -8,4 +8,11 @@ function outputLogData() {
 
     console.log(sam1);
 
+    // オブジェクト2
+    const sam2 = { name: "Sam", age: 4 };
+    console.log(sam2);
+    console.log(sam1.name === sam2.name);
+    console.log(sam1.age === sam2.age);
+    console.log(sam1 === sam2);
+
 }
