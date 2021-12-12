d9081274fecd2c7378061cfdfeaef597a832a6c6
hiro881213 <nikep.godbard@gmail.com>
2021-12-12 21:05:13 +0900

    代入演算子-配列

--- js/chap5/valueOperator.js
+++ js/chap5/valueOperator.js
@@ -32,4 +32,18 @@ function valueOperator() {
     console.log(v);
     console.log(v0);
 
+    console.log("-----代入演算子-配列----");
+
+    const nums = [3, 5, 15, 7, 5];
+
+    let n, i = 0;
+
+    while (i < nums.length && (n = nums[i++]) < 10) {
+
+        console.log(`10より小さい数字を発見:${n}`);
+
+    }
+
+    console.log(`10より小さい数字を発見:${n}`);
+    console.log(`未チェックの数字の個数:${nums.length - i}`);
 }
