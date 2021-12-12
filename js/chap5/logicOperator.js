6702d699d96bc9e990607158f2e54890d91778e4
hiro881213 <nikep.godbard@gmail.com>
2021-12-12 18:02:32 +0900

    論理値偽

--- js/chap5/logicOperator.js
+++ js/chap5/logicOperator.js
@@ -4,4 +4,21 @@ function logicOperator() {
     console.log(3 + 5 + "8");
     console.log("3" + 5 + 8);
 
+    console.log("----論理値----");
+
+    // undefined
+    if (undefined) console.log("t"); else console.log("f");
+
+    // null
+    if (null) console.log("t"); else console.log("f");
+
+    // 0
+    if (0) console.log("t"); else console.log("f");
+
+    // NaN
+    if (NaN) console.log("t"); else console.log("f");
+
+    // ''
+    if ('') console.log("t"); else console.log("f");
+
 }
