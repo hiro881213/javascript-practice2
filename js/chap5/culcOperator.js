a5f4ec31fb74fb6972ef75b990bc5901be65c507
hiro881213 <nikep.godbard@gmail.com>
2021-12-12 16:21:42 +0900

    等価演算子

--- js/chap5/culcOperator.js
+++ js/chap5/culcOperator.js
@@ -1,10 +1,26 @@
 function culcOperator () {
     
+    console.log("-----計算順-----")
+
     let x = 3;
     let y = 0;
 
     x += y = 6*5/2;
 
     console.log(x);
+
+    console.log("-----等価演算子-----")
+
+    const n = 5;
+    const s = "5";
+
+    console.log(n === s);
+    console.log(n !== s);
+
+    console.log(n === Number(s));
+    console.log(n !== Number(s));
+
+    console.log(n == s);
+    console.log(n != s);
 
 }
