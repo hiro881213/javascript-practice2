796847b03888b73007c27de30192da442d788619
hiro881213 <nikep.godbard@gmail.com>
2021-12-12 22:05:17 +0900

    ifelseの省略

--- js/chap5/omitOperator.js
+++ js/chap5/omitOperator.js
@@ -8,4 +8,31 @@ function omitOperator() {
     console.log(`室温と気温の差:${currentTempC - roomTempC}度`);
     console.log(`現在の温度(華氏): ${currentTempC*9/5 + 32}F`);
 
+    console.log("-----ifelseの省略-----");
+
+    // 素数抽出
+    function isPrime(n) {
+        
+        if (n === 2) return true;
+        
+        for (let i = 2; i < n; i++) {
+        
+            if (n % i === 0) return false;
+        
+        }
+        
+        return true;
+    
+    }
+
+    let label;
+    n = 10;
+    nn = 1;
+
+    label = isPrime(n) ? '素数': '非素数';
+    console.log(label);
+
+    label = isPrime(nn) ? '素数': '非素数';
+    console.log(label);
+
 }
