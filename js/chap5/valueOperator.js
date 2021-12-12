7bb0b9fed998c9b502a9b10884a12b4398e7302f
hiro881213 <nikep.godbard@gmail.com>
2021-12-12 20:46:19 +0900

    typeof演算子

--- js/chap5/valueOperator.js
+++ js/chap5/valueOperator.js
@@ -1,10 +1,21 @@
 function valueOperator() {
     
     console.log("-----条件演算子----");
-    
+
     const doIt = false;
     const result = doIt ? "Did it!" : "Didn't do it";
 
     console.log(result);
+
+    console.log("-----typeof演算子----");
+
+    console.log(typeof undefined);
+    console.log(typeof null);
+    console.log(typeof {});
+    console.log(typeof true);
+    console.log(typeof 1);
+    console.log(typeof "");
+    console.log(typeof Symbol());
+    console.log(typeof function(){});
 
 }
