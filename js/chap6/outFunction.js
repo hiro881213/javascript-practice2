893956d1d9eb67fddc75e14bad21ad8300844103
hiro881213 <nikep.godbard@gmail.com>
2021-12-13 23:33:14 +0900

    関数の代入

--- js/chap6/outFunction.js
+++ js/chap6/outFunction.js
@@ -1,18 +1,34 @@
 function outFunction() {
     
+    // コンソール出力関数
     function sayHello() {
         console.log("Hello World");
     }
 
+    console.log("-----関数の実行------");
+
     sayHello();
 
+    // 文字列出力関数
     function getGreeting() {
         return "Hello World!";
     }
 
+    console.log("-----文字列出力------");
+
     const message = getGreeting();
     console.log(message);
-    
+
+    console.log("-----呼び出しと参照------");
+
     console.log(getGreeting());
     console.log(getGreeting);
+
+    console.log("-----関数の代入------");
+
+    const f = getGreeting;
+
+    console.log(f);
+    console.log(f());
+
 }
