b0161b13a820f99faaee39064f16a74249adaf1c
hiro881213 <nikep.godbard@gmail.com>
2021-12-14 23:35:54 +0900

    オブジェクト変更関数

--- js/chap6/argumentFunction.js
+++ js/chap6/argumentFunction.js
@@ -24,4 +24,22 @@ function argumentFunction() {
     f(x);
     console.log(`関数fを呼び出した後:x = ${x}`);
 
+    console.log("-----オブジェクト変更関数------");
+
+    function f2(o) {
+
+        console.log(`関数f2の中、o.messageに代入する前の値:${o.message}`);
+        o.message = "messageの値を関数f2の中で設定した!!";
+        console.log(`関数f2の中、o.messageに代入した後の値:${o.message}`);
+
+    }
+
+    let o = {
+        message: "messageの初期値"
+    };
+
+    console.log(`関数f2を呼び出す前:o.message = ${o.message}`);
+    f2(o);
+    console.log(`関数f2を呼び出した後:o.message = ${o.message}`);
+
 }
