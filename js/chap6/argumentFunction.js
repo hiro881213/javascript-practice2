6f212fa493848ce888ccc34cce6c14356f753ec0
hiro881213 <nikep.godbard@gmail.com>
2021-12-14 23:25:07 +0900

    関数呼び出し

--- js/chap6/argumentFunction.js
+++ js/chap6/argumentFunction.js
@@ -9,4 +9,19 @@ function argumentFunction() {
 
     console.log(avg(5, 10));
 
+    console.log("-----関数呼び出し------");
+
+    function f(x) {
+
+        console.log(`関数fの中(代入の前): x = ${x}`);
+        x = 5;
+        console.log(`関数fの中(代入の後): x = ${x}`);
+
+    }
+
+    let x = 3;
+    console.log(`関数fを呼び出す前:x = ${x}`);
+    f(x);
+    console.log(`関数fを呼び出した後:x = ${x}`);
+
 }
