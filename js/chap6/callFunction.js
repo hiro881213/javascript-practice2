ea7799999e9722936051dfb3b6eec6ac3d022190
hiro881213 <nikep.godbard@gmail.com>
2021-12-18 22:40:36 +0900

    アロー関数

--- js/chap6/callFunction.js
+++ js/chap6/callFunction.js
@@ -19,5 +19,30 @@ function callFunction() {
     }
 
     g(false);
+    
+    console.log("------アロー関数------");
+    
+    const o = {
+        name : 'Julie',
+        greetBackwards: function() {
+
+            const getReverseName = () => {
+                let nameBackwards = '';
+
+                for (let i = this.name.length - 1; i >= 0; i--) {
+                    nameBackwards += this.name[i];
+                }
+
+                return nameBackwards;
+
+            }
+
+            return `${getReverseName()} si eman ym , olleH`;
+
+        },
+
+    };
+
+    console.log(o.greetBackwards());
 
 }
