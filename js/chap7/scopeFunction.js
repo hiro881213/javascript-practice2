7526b25266e6e1d83192574ba20342673362ac91
hiro881213 <nikep.godbard@gmail.com>
2021-12-25 19:13:50 +0900

    静的スコープ2

--- js/chap7/scopeFunction.js
+++ js/chap7/scopeFunction.js
@@ -12,6 +12,21 @@ function scopeFunction() {
 
     f2();
     f1();
+    f2();
+
+    console.log("------静的スコープ2------");
+
+    const x = 3;
+    const y = 5;
+
+    function f3() {
+
+        console.log(x);
+        console.log(y);
+
+    }
+
+    f3();
 
 }
 
