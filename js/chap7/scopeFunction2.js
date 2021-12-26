ec84589ff53594a29d45c32a42c1736cc081654e
hiro881213 <nikep.godbard@gmail.com>
2021-12-26 20:34:52 +0900

    クロージャー2

--- js/chap7/scopeFunction2.js
+++ js/chap7/scopeFunction2.js
@@ -18,4 +18,28 @@ function scopeFunction2() {
 
     globalFunc();
 
+    console.log("------クロージャー2------");
+
+    let f;
+
+    {
+        let o = {
+            note: '安全',
+            note2:'大丈夫'
+        };
+
+        f = function() {
+            console.log("無形関数の中:" + o.note);
+            return o;
+        }
+
+    }
+
+    let oRef = f();
+
+    console.log(oRef);
+
+    oRef.note = "大丈夫ではない";
+    console.log(oRef);
+
 }
