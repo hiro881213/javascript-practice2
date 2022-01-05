a0da86c3bd049ebc80b4df161a17ff6b41acbba1
hiro881213 <nikep.godbard@gmail.com>
2022-01-05 19:18:05 +0900

    オブジェクトの検索

--- js/chap8/arrProcess2.js
+++ js/chap8/arrProcess2.js
@@ -133,4 +133,23 @@ function arrProcess2() {
         console.log(arr.lastIndexOf(true, 3));
     }
 
+    {
+        console.log("------オブジェクトの検索------");
+
+        const arr = [
+            {id: 5, name: "太郎"},
+            {id: 7, name: "花子"}
+        ];
+
+        console.log(arr.findIndex(o => o.id === 5));
+        console.log(arr.findIndex(o => o.name === "花子"));
+        
+        console.log(arr.findIndex(o => o === 3));
+        console.log(arr.findIndex(o => o.id === 17));
+
+        console.log(arr.findIndex(o => o.id === 7));
+
+    }
+
+
 }
