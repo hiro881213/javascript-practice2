f30fd68f158eb4b7a7569c7c6755b07ae868c092
hiro881213 <nikep.godbard@gmail.com>
2022-01-05 20:50:24 +0900

    mapメソッド

--- js/chap8/arrProcess3.js
+++ js/chap8/arrProcess3.js
@@ -10,6 +10,7 @@ function arrProcess3() {
     }
 
     {
+
         console.log("------every------");
 
         const arr = [4, 6, 16, 36];
@@ -18,4 +19,28 @@ function arrProcess3() {
         console.log(arr.every(x => Number.isInteger(Math.sqrt(x))));
 
     }
+
+    {
+        console.log("------mapメソッド------");
+
+        const cart = [
+            {name : "iPhone", price : 54800},
+            {name : "Android",price : 49800}
+        ];
+
+        // 各オブジェクトのnameからなる配列を作る
+        const names = cart.map(x => x.name);
+        console.log(names);
+
+        const prices = cart.map(x => x.price);
+        console.log(prices);
+
+        const discountPrices = prices.map(x => x*0.8);
+        console.log(discountPrices);
+
+        const lcNames = names.map(x => x.toLowerCase());
+        console.log(lcNames);
+
+    }
+
 }
