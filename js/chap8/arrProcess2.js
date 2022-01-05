aedadd5567d14c0c290d090acb53776389cffd74
hiro881213 <nikep.godbard@gmail.com>
2022-01-05 19:30:51 +0900

    findメソッド

--- js/chap8/arrProcess2.js
+++ js/chap8/arrProcess2.js
@@ -151,5 +151,17 @@ function arrProcess2() {
 
     }
 
+    {
 
+        console.log("------findメソッド------");
+
+        const arr = [
+            {id: 5, name: "太郎"},
+            {id: 7, name: "花子"}
+        ];
+
+        console.log(arr.find(o => o.id === 5));
+        console.log(arr.find(o => o.id === 2));
+
+    }
 }
