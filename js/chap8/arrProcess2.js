400fc5664ea9b10928fc4971514039a680d83115
hiro881213 <nikep.godbard@gmail.com>
2022-01-05 18:29:55 +0900

    オブジェクトのソート

--- js/chap8/arrProcess2.js
+++ js/chap8/arrProcess2.js
@@ -82,4 +82,26 @@ function arrProcess2() {
 
     }
 
+    {
+        console.log("------オブジェクトのソート------");
+
+        let arr = [
+            {name: "Suzanne"},
+            {name: "Jim"},
+            {name: "Trevor"},
+            {name: "Amanda"},
+        ];
+
+        console.log(arr);
+
+        arr.sort((a, b) => a.name > b.name ? 1 : (a.name === b.name ? 0 : 1));
+        console.log("-----");
+        console.log(arr);
+
+        arr.sort((a, b) => a.name[1] < b.name[1] ? 1 : (a.name[1] === b.name[1] ? 0 : -1));
+        console.log("-----");
+        console.log(arr);
+
+    }
+
 }
