1ad197b951139c5fca2df7415c435d7721d802c5
hiro881213 <nikep.godbard@gmail.com>
2022-01-05 18:00:20 +0900

    fillメソッド

--- js/chap8/arrProcess2.js
+++ js/chap8/arrProcess2.js
@@ -23,4 +23,28 @@ function arrProcess2() {
 
     }
 
+    {
+        console.log("------Fillメソッド------");
+
+        // 数値を初期値に設定する
+        let arr = new Array(5).fill(1);
+        console.log(arr);
+
+        // 文字列を初期値に設定する
+        let arr2 = arr.fill("a");
+        console.log(arr2);
+
+        // arr[1]から最後まで"b"を代入する
+        console.log(arr.fill("b", 1));
+
+        // arr[2]からarr[4]の前まで"c"を代入する
+        console.log(arr.fill("c", 2, 4));
+
+        // 最後から4番目の要素から最後まで5.5を代入する
+        console.log(arr.fill(5.5, -4));
+
+        // 最後から3番目の要素から最後の一つ前まで0を代入
+        console.log(arr.fill(0, -3, -1));
+
+    }
 }
