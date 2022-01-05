071f4a862f6fa4ffa254f956a77b97d9c1a1d6bb
hiro881213 <nikep.godbard@gmail.com>
2022-01-05 18:10:57 +0900

    ソート

--- js/chap8/arrProcess2.js
+++ js/chap8/arrProcess2.js
@@ -65,4 +65,21 @@ function arrProcess2() {
         console.log(arr2);
 
     }
+
+    {
+        console.log("------ソート------");
+
+        let arr = [5, 4, 3, 2, 1];
+        let arr2 = arr.sort();
+
+        console.log(arr);
+        console.log(arr2);
+
+        arr2.reverse();
+
+        console.log(arr);
+        console.log(arr2);
+
+    }
+
 }
