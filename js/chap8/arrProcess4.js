1a96eda4e4f9de7fe45f51384b9ebb480723cb2c
hiro881213 <nikep.godbard@gmail.com>
2022-01-08 22:07:39 +0900

    Joinメソッド

--- js/chap8/arrProcess4.js
+++ js/chap8/arrProcess4.js
@@ -15,4 +15,24 @@ function arrProcess4() {
 
     }
 
+    {
+        
+        console.log("------Joinメソッド------");
+
+        const arr = [1, null, "hello", "world", true, undefined];
+
+        // arr[3]を削除する
+        delete arr[3];
+
+        // joinメソッドを実行する
+        let result = arr.join();
+        console.log(result);
+
+        result = arr.join('');
+        console.log(result);
+
+        result = arr.join(" -- ");
+        console.log(result);
+
+    }
 }
