628911171560854eace07dcdc6e8c79398af959d
hiro881213 <nikep.godbard@gmail.com>
2022-01-08 21:28:43 +0900

    reduceメソッド2

--- js/chap8/arrProcess3.js
+++ js/chap8/arrProcess3.js
@@ -139,4 +139,18 @@ function arrProcess3() {
 
     }
 
+    {
+        console.log("------reduceメソッド2------");
+
+        // 配列を生成する
+        const arr = [5, 7, 2, 4];
+
+        const sum = arr.reduce((a, x) => a += x);
+        console.log(sum);
+
+        const sum2 = arr.reduce((a, x) => a + x);
+        console.log(sum2);
+
+    }
+
 }
