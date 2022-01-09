1c61d72469e98911e91a21a4fea2720477b4d9ce
hiro881213 <nikep.godbard@gmail.com>
2022-01-09 21:58:59 +0900

    アクセッサプロパティ

--- js/chap9/object-orient.js
+++ js/chap9/object-orient.js
@@ -86,7 +86,7 @@ function objectOrient () {
         console.log("------クラスオブジェクトの利用------");
 
         class Car {
-            
+
             constructor(make, model) {
 
                 // メーカー
@@ -126,6 +126,61 @@ function objectOrient () {
 
         console.log(car1.userGear);
         console.log(car2.userGear);
+
+    }
+
+    {
+
+        console.log("------アクセッサプロパティ------");
+
+        class Car {
+
+            // コンストラクタを定義する
+            constructor(make, model) {
+
+                // メーカー
+                this.make = make;
+                
+                // モデル
+                this.model = model;
+                
+                // ギア
+                this._userGears = ['P', 'N', 'R', 'D'];
+
+                // 選択ギア
+                this._userGear = this._userGears[0];
+
+
+            }
+
+            get userGear() { return this._userGear; }
+
+            set userGear(value) {
+
+                if (this._userGears.indexOf(value) < 0)
+                    throw new Error(`ギア指定が正しくない: ${value}`);
+
+                this._userGear = value;
+            
+            }
+
+            shift(gear) { this.userGear = gear; }
+
+        }
+
+        const car1 = new Car("Tesla", "Model S");
+        const car2 = new Car("Mazda", "3i");
+
+        console.log(car1);
+        console.log(car2);
+        
+        car1.shift('D');
+        car2.shift('R');
+
+        console.log(car1.userGear);
+        console.log(car2.userGear);
+
+        car1.userGear = "X";
 
     }
 
