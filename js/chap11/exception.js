08e447026ebbcd67f3549008fc7bfb325a8a7487
hiro881213 <nikep.godbard@gmail.com>
2022-01-18 22:13:38 +0900

    Errorオブジェクトの利用2

--- js/chap11/exception.js
+++ js/chap11/exception.js
@@ -1,7 +1,7 @@
 function procException() {
     {
 
-        console.log('Errorオブジェクト');
+        console.log('------Errorオブジェクト------');
 
         // const email = "test@test.com";
         const email = "testtest.com";
@@ -10,7 +10,7 @@ function procException() {
 
         if (validateEmail instanceof Error) {
 
-            console.log(validateEmail.message);
+            console.error(validateEmail.message);
 
         }  else {
             console.log(`正しい形式のアドレス: ${validateEmail}`);
@@ -22,6 +22,53 @@ function procException() {
                     email: new Error(`無効なメールアドレスです`)
         }
 
+    }
+
+    {
+
+        console.log("------try~catch------");
+    
+        function ValidateEmail(email) {
+
+            try {
+
+                return email.match(/@/) ? email : new Error(`無効なメールアドレスです`);
+
+            } catch (err) {
+
+                console.error(`validateEmail: ${err.message}`);
+                throw new Error('エラーが起こった');
+
+            }
+
+        }
+
+        const email = 'AAAA@test.com';
+        // const email = 'AAAA';
+        // const email = null;
+
+        try {
+            
+            const validatedEmail = ValidateEmail(email);
+
+            console.log(validatedEmail);
+
+            if (validatedEmail instanceof Error) {
+
+                console.error(validatedEmail.message);
+
+            } else { 
+                
+                console.log(`正しい形式のアドレス: ${validatedEmail}`);
+
+            }
+
+        } catch (err) {
+
+            console.error(`エラー:${err.message}`);
+
+        }
 
     }
+
 }
