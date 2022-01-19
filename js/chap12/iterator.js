ef068a52b3bbe560e3dea6dacf35af31e2585111
hiro881213 <nikep.godbard@gmail.com>
2022-01-19 22:00:08 +0900

    for~Of処理

--- js/chap12/iterator.js
+++ js/chap12/iterator.js
@@ -1,3 +1,22 @@
 function procIterator() {
-    alert();
+    
+    {
+        console.log("------for~Of処理------");
+
+        const book = [
+            "A本",
+            "B本",
+            "C本",
+            "D本",
+            "E本",
+            "F本",
+            "G本"
+        ];
+
+        for (let aPage of book) {
+            console.log(aPage);
+        }
+
+    }
+
 }
