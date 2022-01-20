861de3d9495fffa1872204ddca86141b3011644d
hiro881213 <nikep.godbard@gmail.com>
2022-01-20 22:16:51 +0900

    ジェネレータ生成

--- js/chap12/generator.js
+++ js/chap12/generator.js
@@ -1,3 +1,18 @@
 function procGenerator() {
-    alert();
+    {
+
+        // ジェネレータを生成する
+        function* rainbow() {
+            yield '赤';
+            yield '橙';
+            yield '黄';
+            yield '緑';
+            yield '青';
+            yield '水';
+            yield '紫';
+        }
+
+
+    }
+
 }
