003632544162302c8463bc40718d2770f727d9b6
hiro881213 <nikep.godbard@gmail.com>
2022-01-25 02:30:29 +0900

    純粋関数

--- js/chap13/subroutine.js
+++ js/chap13/subroutine.js
@@ -50,4 +50,35 @@ function procSubroutine() {
 
     }
 
+    {
+
+        console.log("------純粋関数------");
+
+        const colors = [
+            '赤','橙','黄','緑','青','水色','紫'
+        ];
+
+        let colorIndex = -1;
+
+        function getNextRainbowColor() {
+
+            if (++colorIndex >= colors.length) colorIndex = 0;
+            return colors[colorIndex];
+
+        }
+
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+
+    }
+
 }
