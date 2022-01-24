3ee149c00c4c089471680f181b5abe90d661d49a
hiro881213 <nikep.godbard@gmail.com>
2022-01-25 01:43:35 +0900

    サブルーチン

--- js/chap13/subroutine.js
+++ js/chap13/subroutine.js
@@ -1,3 +1,16 @@
 function procSubroutine() {
-    alert();
+    {
+
+        console.log("------サブルーチン------")
+
+        // システム年を取得する
+        const year = new Date().getFullYear();
+
+        if (year % 4 !== 0) console.log(`${year}年は閏年ではない`)
+        else if (year % 100 !== 0) console.log(`${year}年は閏年である`)
+        else if (year % 400 !== 0) console.log(`${year}年は閏年ではない`)
+        else console.log(`${year}年は閏年である`);
+
+    }
+
 }
