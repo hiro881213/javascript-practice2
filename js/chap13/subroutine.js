d93a12517b02144824ecdb414f0579d8a0537840
hiro881213 <nikep.godbard@gmail.com>
2022-01-25 02:12:10 +0900

    サブルーチンの関数化

--- js/chap13/subroutine.js
+++ js/chap13/subroutine.js
@@ -13,4 +13,23 @@ function procSubroutine() {
 
     }
 
+    {
+
+        console.log("------サブルーチンの関数化------");
+
+        function printLeapYearStatus() {
+
+            const year = new Date().getFullYear();
+        
+            if (year % 4 !== 0) console.log(`${year}年は閏年ではない`)
+            else if (year % 100 !== 0) console.log(`${year}年は閏年である`)
+            else if (year % 400 !== 0) console.log(`${year}年は閏年ではない`)
+            else console.log(`${year}年は閏年である`);
+
+        }
+
+        printLeapYearStatus();
+
+    }
+
 }
