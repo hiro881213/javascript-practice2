650584e27a33d95cc00b3d25fc9c2386af50508f
hiro881213 <nikep.godbard@gmail.com>
2022-01-25 02:26:02 +0900

    値を返す関

--- js/chap13/subroutine.js
+++ js/chap13/subroutine.js
@@ -32,4 +32,22 @@ function procSubroutine() {
 
     }
 
+    {
+        console.log("------値を返す関数------");
+
+        function isCurrentYearLeap() {
+
+            const year = new Date().getFullYear();
+
+            if (year % 4 !== 0) return false;
+            else if (year % 100 != 0) return true;
+            else if (year % 400 != 0) return false;
+            else return true;
+
+        }
+
+        console.log(isCurrentYearLeap());
+
+    }
+
 }
