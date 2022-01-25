a2345bc50312250614e7c76d3952d64ffe68094d
hiro881213 <nikep.godbard@gmail.com>
2022-01-26 02:54:30 +0900

    関数はオブジェクト

--- js/chap13/procFunc.js
+++ js/chap13/procFunc.js
@@ -1,3 +1,17 @@
 function procFunc() {
-    alert();
+    
+    {
+        
+        console.log("------関数はオブジェクト------");
+
+        const v = function() {};
+
+        const a = [1, 2, 3];
+
+        console.log(typeof(v));
+        console.log(typeof(a));
+        console.log(v instanceof Object);
+
+    }
+
 }
