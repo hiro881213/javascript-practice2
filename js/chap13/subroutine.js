8ef1cb125e73bdde115665dc66bc77b3429278a7
hiro881213 <nikep.godbard@gmail.com>
2022-01-26 02:22:56 +0900

    純粋関数2

--- js/chap13/subroutine.js
+++ js/chap13/subroutine.js
@@ -96,4 +96,39 @@ function procSubroutine() {
 
     }
 
+    {
+
+        console.log("------純粋関数2------");
+
+        const getNextRainbowColor = (function() {
+
+            const colors =[
+                '赤','橙','黄','緑','青','青','水色','紫'
+            ];
+
+            let colorIndex = -1;
+
+            return function() {
+
+                if (++colorIndex >= colors.length) colorIndex = 0;
+                return colors[colorIndex];
+            
+            }
+
+        })();
+        
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+        console.log(getNextRainbowColor());
+
+    }
+
 }
