a2e84c4ba604defa06a41f6c463138831bbfa2ad
hiro881213 <nikep.godbard@gmail.com>
2022-01-29 19:32:35 +0900

    再帰

--- js/chap13/pipeProc.js
+++ js/chap13/pipeProc.js
@@ -94,4 +94,23 @@ function pipeProc() {
 
     }
 
+    {
+
+        console.log("------再帰------");
+        
+        function findNeedle(haystack) {
+
+            if ( haystack.length === 0 ) return "藁山はなくなった";
+            if ( haystack.shift() === '針') return "針が見つかった";
+            
+            console.log(haystack);
+
+            return findNeedle(haystack);
+
+        }
+
+        console.log(findNeedle(['藁','藁','藁','藁','藁','藁','針','藁']));
+
+    }
+
 }
