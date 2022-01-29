a55281a1b4147aa553ae8459106467084df49799
hiro881213 <nikep.godbard@gmail.com>
2022-01-29 19:03:14 +0900

    関数に関数を渡す

--- js/chap13/pipeProc.js
+++ js/chap13/pipeProc.js
@@ -61,4 +61,21 @@ function pipeProc() {
 
     }
 
+    {
+
+        console.log("------関数に関数を渡す------");
+
+        function sum(arr, f) {
+
+            if (typeof f != 'function') f = x => x;
+
+            return arr.reduce((a, x) => a += f(x), 0);
+
+        }
+
+        console.log(sum([1, 2, 3]));
+        console.log(sum([1, 2, 3], x => x * x));
+        console.log(sum([1, 2, 3], x => Math.pow(x, 3)));
+    }
+
 }
