54d43cbf8c8c3d684ba62387db33fe74595ce15f
hiro881213 <nikep.godbard@gmail.com>
2022-01-29 18:25:51 +0900

    数値計算2

--- js/chap13/procFunc.js
+++ js/chap13/procFunc.js
@@ -111,6 +111,10 @@ function procFunc() {
         const answer1 = (addThreeSquareAddFiveTakeSquareRoot(5) + addThreeSquareAddFiveTakeSquareRoot(2))/addThreeSquareAddFiveTakeSquareRoot(7);
         console.log(answer1);
 
+        const f = addThreeSquareAddFiveTakeSquareRoot;
+        const answer2 = (f(5) + f(2)) / f(7);
+
+        console.log(answer2);
 
     }
 
