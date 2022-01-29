111966866c8d45b60d644e0f5e166e7de1de2591
hiro881213 <nikep.godbard@gmail.com>
2022-01-29 18:24:15 +0900

    数値計算

--- js/chap13/procFunc.js
+++ js/chap13/procFunc.js
@@ -97,4 +97,21 @@ function procFunc() {
         }
 
     }
+
+    {
+
+        console.log("------関数と変数------");
+
+        function addThreeSquareAddFiveTakeSquareRoot(x) {
+
+            return Math.sqrt(Math.pow(x + 3, 2) + 5);
+
+        }
+
+        const answer1 = (addThreeSquareAddFiveTakeSquareRoot(5) + addThreeSquareAddFiveTakeSquareRoot(2))/addThreeSquareAddFiveTakeSquareRoot(7);
+        console.log(answer1);
+
+
+    }
+
 }
