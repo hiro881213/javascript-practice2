0e99c5c4a8c3b95b72047f3802c6ee0e64524690
hiro881213 <nikep.godbard@gmail.com>
2022-01-31 23:19:47 +0900

    Promise処理

--- js/chap14/promiseFunc.js
+++ js/chap14/promiseFunc.js
@@ -1,5 +1,30 @@
 function promiseFunc() {
+    {
 
-    console.log('------Promise処理------');
+        console.log('------Promise処理------');
+
+        function countdown(seconds) {
+
+            return new Promise((onFulfilled, onRejected) => {
+
+                for (let i = seconds; i >= 0; i--){
+
+                    setTimeout(() => {
+
+                        if (i > 0) console.log(i + '...');
+                        else onFulfilled(console.log('GO!'));
+                        
+
+                    });
+
+                }
+
+            })
+
+        }
+
+        countdown(5);
+
+    }
 
 }
