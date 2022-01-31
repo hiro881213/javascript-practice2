a49342e6f201319d07f4c540f3aaeae4f8831bfa
hiro881213 <nikep.godbard@gmail.com>
2022-02-01 03:22:27 +0900

    エラーの発生

--- js/chap14/promiseFunc.js
+++ js/chap14/promiseFunc.js
@@ -66,4 +66,54 @@ function promiseFunc() {
 
     }
 
+    {
+        
+        console.log('------エラーの発生------');
+
+        function countdown(seconds) {
+
+            return new Promise((onFulfilled, onRejected) => {
+
+                const timeoutIds = [];
+
+                for (let i = seconds; i>= 0; i--) {
+
+                    timeoutIds.push(setTimeout(() => {
+
+                        if ( i === 13 ) {
+
+                            timeoutIds.forEach(clearTimeout);
+                            
+                            // エラー関数を読み込む
+                            onRejected(new Error(`${i}という数字は不吉です`));
+    
+                        } else if (i > 0) {
+    
+                            console.log(i + '...');
+    
+                        } else {
+    
+                            console.log('GO!');
+                            
+                            // 成功関数を読み込む
+                            onFulfilled();
+    
+                        }
+    
+
+                    },(seconds - i) * 1000));
+
+                }
+
+            });
+
+        }
+
+        countdown(15).then(
+            () => console.log('カウントダウン成功'),
+            (err) => console.log(`カウントダウンでエラーが起こった: ${err.message}`)
+        );
+
+    }
+
 }
