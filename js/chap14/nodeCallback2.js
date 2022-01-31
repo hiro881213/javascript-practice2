d0eb8203d54c955f7b08d1127c3c0823b2e5380c
hiro881213 <nikep.godbard@gmail.com>
2022-01-31 20:54:55 +0900

    nodeJSコールバック2

---
+++ js/chap14/nodeCallback2.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+fs.readFile('a.txt', (err, dataA) => {
+    
+    if (err) console.error(err);
+
+    fs.readFile('b.txt', (err, dataB) => {
+        if (err) console.error(err);
+
+        fs.readFile('c.txt', (err,dataC) => {
+
+            if (err) console.error(err);
+
+            fs.writeFile('d.txt', dataA + dataB + dataC, (err) => {
+                if (err) console.err(err);
+            });
+
+        });
+
+    });
+
+});
