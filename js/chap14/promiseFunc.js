7d126524bd63cf3ce593f782ed9ee6988ce418be
hiro881213 <nikep.godbard@gmail.com>
2022-02-01 12:22:09 +0900

    プロミスのチェイニング

--- js/chap14/promiseFunc.js
+++ js/chap14/promiseFunc.js
@@ -116,4 +116,68 @@ function promiseFunc() {
 
     }
 
+    {
+
+        console.log('------プロミスのチェイニング------');
+
+        function countdown(seconds) {
+
+            return new Promise((onFulfilled, onRejected) => {
+
+                const timeoutIds = [];
+
+                for (let i = seconds; i >= 0; i--){
+
+                    timeoutIds.push(setTimeout(() => {
+
+                        if (i === 13) {
+
+                            // すべてクリア
+                            timeoutIds.forEach(clearTimeout);
+
+                            return onRejected(new Error(`${i}という数は不吉過ぎます`));
+
+                        }
+
+                        if (i > 0) console.log(i + '...');
+                        else onFulfilled(console.log('GO!!'));
+
+                    }, (seconds - i) * 1000));
+
+                }
+
+            })
+
+        }
+
+        function launch() {
+
+            return new Promise ((onFulfilled, onRejected)=> {
+
+                console.log('発射');
+
+                setTimeout(() => {
+
+                    onFulfilled('周回軌道に乗った');
+
+                }, 2 * 1000);
+
+            });
+
+        }
+
+        countdown(15)
+            .then(launch)
+            .then((msg) => {
+                console.log(msg)
+            })
+            .catch((err) => {
+
+                console.error('トラブル発生')
+
+            });
+
+
+    }
+
 }
