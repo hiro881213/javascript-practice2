8f905528e5d0bd1d7e4e584a2a1765f5399bb576
hiro881213 <nikep.godbard@gmail.com>
2022-02-04 00:27:49 +0900

    ファイル入出力とジェネレータ作成

--- js/chap14/nodeGeneratorRunner.js
+++ js/chap14/nodeGeneratorRunner.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 console.log('------ジェネレータランナー------');
 
+// ジェネレータランナー処理
 function grun(g) {
 
     const it = g();
@@ -22,5 +23,51 @@ function grun(g) {
         }
 
     })();
+
+}
+
+// ファイル読み込み処理
+function readFile(fileName) {
+
+    return new Promise(
+        (onFulfilled, onRejected) => {
+
+            // ファイル読み込み処理を実行する
+            fs.readFile(
+                fileName,
+                'utf-8',
+                (err, data) => err ? onRejected(err) : onFulfilled(data));
+        }
+    );
+
+}
+
+// ファイル書き込み処理
+function writeFile() {
+
+    return new Promise(
+        
+        (onFulfilled, onRejected) => {
+
+            // ファイル書き込み処理を実行する
+            fs.writeFile(
+                fileName,
+                data,
+                err => err ? onRejected(err) : onFulfilled('OK'));
+
+        }
+
+    );
+
+}
+
+// ジェネレータ生成
+function* fileReadAndWrite() {
+
+    const dataA = yield readFile('a.txt');
+    const dataB = yield readFile('b.txt');
+    const dataC = yield readFile('c.txt');
+
+    yield writeFile('gd.txt', dataA + dataB + dataC);
 
 }
