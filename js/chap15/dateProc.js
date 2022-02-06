63ddf92bf2b55e59f2bc70e8da7e84a5b65804b6
hiro881213 <nikep.godbard@gmail.com>
2022-02-06 23:40:57 +0900

    Date関数

--- js/chap15/dateProc.js
+++ js/chap15/dateProc.js
@@ -13,4 +13,24 @@ function dateProc() {
 
     }
 
+
+    {
+
+        console.log('------Dateオブジェクトの生成------');
+
+        // Dateインスタンスを生成する
+        let d = new Date();
+
+        console.log(d);
+
+        // 2017/01/01 00:00:00
+        d = new Date(2017, 0);
+        console.log(d);
+
+        // 2017/02/01 00:00:00
+        d = new Date(2017, 1);
+        console.log(d);
+
+
+    }
 }
