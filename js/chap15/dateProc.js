ace54778b7fb4c80701b3399353d5bd346d88ace
hiro881213 <nikep.godbard@gmail.com>
2022-02-06 23:51:21 +0900

    Dateオブジェクトの生成

--- js/chap15/dateProc.js
+++ js/chap15/dateProc.js
@@ -18,18 +18,37 @@ function dateProc() {
 
         console.log('------Dateオブジェクトの生成------');
 
+        function consoleFunc(str) {
+
+            console.log(str);
+
+        }
+
         // Dateインスタンスを生成する
         let d = new Date();
 
         console.log(d);
 
         // 2017/01/01 00:00:00
-        d = new Date(2017, 0);
-        console.log(d);
+        consoleFunc(new Date(2017, 0));
 
         // 2017/02/01 00:00:00
-        d = new Date(2017, 1);
-        console.log(d);
+        consoleFunc(new Date(2017, 1));
+
+        // 2017/02/14 00:00:00
+        consoleFunc(new Date(2017, 1,14));
+
+        // 2017/02/14 13:00:00
+        consoleFunc(new Date(2017, 1, 14, 13));
+
+        // 2017/02/14 13:30:00
+        consoleFunc(new Date(2017, 1, 14,13, 30));
+
+        // 2017/02/14 13:30:05
+        consoleFunc(new Date(2017, 1, 14, 13, 30, 5));
+
+        // 2017/02/14 13:30:05:05
+        consoleFunc(new Date(2017, 1, 14, 13, 30, 5, 500));
 
 
     }
