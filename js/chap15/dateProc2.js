d6be728da7697cbc4d15acb861276110385b113d
hiro881213 <nikep.godbard@gmail.com>
2022-02-13 00:35:20 +0900

    Dateオブジェクトの文字列化

--- js/chap15/dateProc2.js
+++ js/chap15/dateProc2.js
@@ -1,3 +1,19 @@
 function dateProc2() {
-    alert();
+    
+    console.log('------Dateオブジェクトの文字列化------');
+
+    const d = new Date(Date.UTC(1930, 4, 10, 10, 0));
+
+    // 1930/05/10
+    console.log(d.toLocaleDateString());
+
+    // 19:00:00
+    console.log(d.toLocaleTimeString());
+
+    // 19:00:00 GMT+0900 (日本標準時)
+    console.log(d.toTimeString());
+
+    // Sat, 10 May 1930 10:00:00 GMT
+    console.log(d.toUTCString());
+
 }
