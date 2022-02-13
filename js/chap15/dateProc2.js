532b0931e95565b315a0f8ffb27b4b5492d88669
hiro881213 <nikep.godbard@gmail.com>
2022-02-13 17:00:08 +0900

    Dateオブジェクトの時分秒取得

--- js/chap15/dateProc2.js
+++ js/chap15/dateProc2.js
@@ -1,3 +1,30 @@
 function dateProc2() {
-    alert();
+    
+    console.log('------Dateオブジェクトの時分秒取得------');
+    const d = new Date(Date.UTC(1815, 9, 10));
+
+    // 1815
+    console.log(d.getFullYear());
+
+    // 9
+    console.log(d.getMonth());
+
+    // 10
+    console.log(d.getDate());
+
+    // 2
+    console.log(d.getDay());
+
+    // 9
+    console.log(d.getHours());
+
+    // 18
+    console.log(d.getMinutes());
+
+    // 59
+    console.log(d.getSeconds());
+
+    // 0
+    console.log(d.getMilliseconds());
+
 }
