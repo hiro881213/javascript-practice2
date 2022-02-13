b705c01e413739ddc4095dc57cdd3cc00858a0a4
hiro881213 <nikep.godbard@gmail.com>
2022-02-13 17:07:09 +0900

    UTCの時分秒取得

--- js/chap15/dateProc2.js
+++ js/chap15/dateProc2.js
@@ -1,6 +1,7 @@
 function dateProc2() {
     
     console.log('------Dateオブジェクトの時分秒取得------');
+
     const d = new Date(Date.UTC(1815, 9, 10));
 
     // 1815
@@ -26,5 +27,31 @@ function dateProc2() {
 
     // 0
     console.log(d.getMilliseconds());
+
+    console.log('------UTCの時分秒取得------');
+
+    // 1815
+    console.log(d.getUTCFullYear());
+
+    // 9(→10月)
+    console.log(d.getUTCMonth());
+
+    // 10
+    console.log(d.getUTCDate());
+
+    // 2(→火曜日)
+    console.log(d.getUTCDay());
+
+    // 0
+    console.log(d.getUTCHours());
+
+    // 0
+    console.log(d.getUTCMinutes());
+
+    // 0
+    console.log(d.getUTCSeconds());
+
+    // 0
+    console.log(d.getUTCMilliseconds());
 
 }
