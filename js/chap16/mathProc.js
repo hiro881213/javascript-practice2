9d94c7b02308ef567532044f2b5bd6515bc968c1
hiro881213 <nikep.godbard@gmail.com>
2022-02-13 19:12:35 +0900

    固定小数点

--- js/chap16/mathProc.js
+++ js/chap16/mathProc.js
@@ -1,3 +1,23 @@
 function mathProc() {
-    alert();
+    
+    {
+
+        console.log('------固定小数点------');
+
+        const x = 19.51;
+
+        // 19.510
+        console.log(x.toFixed(3));
+
+        // 19.51
+        console.log(x.toFixed(2));
+        
+        // 19.5
+        console.log(x.toFixed(1));
+
+        // 20
+        console.log(x.toFixed(0));
+
+    }
+
 }
