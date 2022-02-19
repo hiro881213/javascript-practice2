f196b54f804bd987fe88792edacd4cfcd7929aeb
hiro881213 <nikep.godbard@gmail.com>
2022-02-19 19:29:32 +0900

    1+xの自然対数

--- js/chap16/mathProc.js
+++ js/chap16/mathProc.js
@@ -162,6 +162,10 @@ function mathProc() {
         console.log(Math.log2(2));
         console.log(Math.log2(5));
 
+        console.log('------1+xの自然対数------');
+
+        console.log(Math.log1p(Math.E - 1));
+        console.log(Math.log1p(17.5));
 
     }
 
