96483559f974634db01f15255c34d9f9e5611975
hiro881213 <nikep.godbard@gmail.com>
2022-02-23 19:49:59 +0900

    単純マッチング（日文）

--- js/chap17/ProcRegularExpressions1.js
+++ js/chap17/ProcRegularExpressions1.js
@@ -1,6 +1,6 @@
 function ProcRegularExpressions1() {
     
-    console.log("------正規表現(英文)------");
+    console.log("------単純マッチング(英文)------");
 
     const input = "As I was going to Saint Ives";
 
@@ -16,5 +16,20 @@ function ProcRegularExpressions1() {
     console.log(input.indexOf("going"));
     console.log(input.indexOf("going", 10));
     console.log(input.indexOf("nope"));
+
+    console.log("------単純マッチング（日文）------");
+
+    const input2 = "セント・アイヴスはイギリスにある町の名前です";
+
+    console.log(input2.startsWith("セント"));
+    console.log(input2.endsWith("です"));
+
+    console.log(input2.startsWith("イギリス", 9));
+
+    console.log(input2.includes("アメリカ"));
+    console.log(input2.includes("町", 10));
+    console.log(input2.indexOf("町"));
+    console.log(input2.indexOf("町", 10));
+    console.log(input2.indexOf("アメリカ"));
 
 }
