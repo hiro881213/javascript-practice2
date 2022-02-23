53ad7eac93aeaa5c5411ad5d022de438363b167c
hiro881213 <nikep.godbard@gmail.com>
2022-02-23 19:42:49 +0900

    単純マッチング（英文）

--- js/chap17/ProcRegularExpressions1.js
+++ js/chap17/ProcRegularExpressions1.js
@@ -1,3 +1,20 @@
 function ProcRegularExpressions1() {
-    alert();
+    
+    console.log("------正規表現(英文)------");
+
+    const input = "As I was going to Saint Ives";
+
+    console.log(input.startsWith("As"));
+    console.log(input.endsWith("Ives"));
+
+    console.log(input.startsWith("going", 9));
+    console.log(input.endsWith("going",  14));
+
+    console.log(input.includes("going"));
+    console.log(input.includes("going", 10));
+
+    console.log(input.indexOf("going"));
+    console.log(input.indexOf("going", 10));
+    console.log(input.indexOf("nope"));
+
 }
